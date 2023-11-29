9451e8603b795105f8d4e8196e763df79bfb8886
Aidan McCarthy <aidanmccarthy614@outlook.com>
2023-11-29 18:38:24 +0000

    added assets and click handler code

--- TileGame/Game1.cs
+++ TileGame/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -8,7 +9,8 @@ namespace TileGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        Texture2D footballTexture;
+        Texture2D tennisballTexture;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -26,7 +28,8 @@ namespace TileGame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            footballTexture = Content.Load<Texture2D>("ball");
+            tennisballTexture = Content.Load<Texture2D>("tennisball");
             //this.Content - load game content 
         }
 
@@ -34,7 +37,32 @@ namespace TileGame
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            MouseState mouseState = Mouse.GetState();
 
+            if (mouseState.LeftButton == ButtonState.Released)
+            {
+                Point mousePosition = new Point(mouseState.X, mouseState.Y);
+
+                // Check if the mouse is over the first image
+                Rectangle image1Bounds = new Rectangle(100, 100, footballTexture.Width, footballTexture.Height);
+                if (image1Bounds.Contains(mousePosition))
+                {
+                    // Action to perform when the first image is clicked
+                    // Example: Change a variable's value, trigger an event, etc.
+                    // Add your custom logic here
+                    System.Diagnostics.Debug.WriteLine("Football Clicked!"); 
+                }
+
+                // Check if the mouse is over the second image
+                Rectangle image2Bounds = new Rectangle(200, 100, tennisballTexture.Width, tennisballTexture.Height);
+                if (image2Bounds.Contains(mousePosition))
+                {
+                    // Action to perform when the second image is clicked
+                    // Example: Play a sound, transition to another screen, etc.
+                    // Add your custom logic here
+                    System.Diagnostics.Debug.WriteLine("Tennisball clicked!");
+                }
+            }
             // update logic
 
             base.Update(gameTime);
@@ -43,6 +71,17 @@ namespace TileGame
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            // Draw the sprite at a specific position (for example, at coordinates (100, 100))
+            _spriteBatch.Draw(footballTexture, new Vector2(100, 100), Color.White);
+            _spriteBatch.Draw(tennisballTexture, new Vector2(200, 100), Color.White);
+
+            _spriteBatch.End();
+
+
+            // You'll draw these rectangles in the Draw method using _spriteBatch.DrawRectangle(...)
 
             // drawing code 
 
